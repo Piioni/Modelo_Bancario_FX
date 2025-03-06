dba5691ccbb1cd0d5d4e83d1647623aaeb58d7f6
Piioni <rangelc.juane@gmail.com>
2025-03-06 21:18:13 +0100

    Vinculate GUI to @SpringBootApplication

--- src/main/java/foc/es/banco/SpringApp.java
+++ src/main/java/foc/es/banco/SpringApp.java
@@ -1,14 +1,33 @@
 package foc.es.banco;
 
+import foc.es.banco.gui.SceneController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SpringApp  {
+public class SpringApp extends Application {
+    private ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringApp.class, args);
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        context = SpringApplication.run(SpringApp.class);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        SceneController sceneController = context.getBean(SceneController.class);
+        sceneController.loadApplication(primaryStage);
+    }
+
+    @Override
+    public void stop() {
+        context.close();
     }
 }
